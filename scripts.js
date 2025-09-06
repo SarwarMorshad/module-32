30ec8a226b48e3293853de281dc5b11076e27cc5
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-09-06 11:08:53 +0200

    Module 32: Displaying post

--- scripts.js
+++ scripts.js
@@ -1,11 +1,26 @@
-function loadData() {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-}
+// function loadData() {
+//   fetch("https://jsonplaceholder.typicode.com/todos/1")
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// }
 
 document.getElementById("loadDataButton").addEventListener("click", function () {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
     .then((response) => response.json())
     .then((json) => console.log(json));
 });
+
+document.getElementById("btn-load-post").addEventListener("click", function () {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res) => res.json())
+    .then((data) => displayPost(data));
+});
+
+const displayPost = (posts) => {
+  const postsContainer = document.getElementById("post-container");
+  posts.forEach((post) => {
+    const li = document.createElement("li");
+    li.textContent = post.title;
+    postsContainer.appendChild(li);
+  });
+};
